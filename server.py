6692a27936ee3390c7bb93677f6e236c3e513f0b
Naman Jhanjhari <naman.jhanjhari@visiblealpha.com>
2020-01-03 20:08:02 -0800

    Adding std files

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ class HelloWorld(object):
         date = r.get('date')
         date_str = datetime.strptime(date, '%d%m%y').strftime("%b %d %Y")
         if date != datetime.now().strftime("%d%m%y"):
-            os.system("python extract_and_dump_bhav_copy.py")
+            os.system("python extract_and_dump_bhav_copy.py > script_std.log")
         top_10_ids = r.sort("keys",alpha=True,start=0,num=10)
         for key in top_10_ids:
             li.append(r.hgetall(key))
